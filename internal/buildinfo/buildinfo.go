99832642344f8c5620947a31d1d12049d3886b0f
rkclayton <56836047+rkclayton@users.noreply.github.com>
2026-09-07 14:51:00 -0500

    chore: finalize v0.9.0 release metadata

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -14,7 +14,7 @@ import (
 var (
 	Commit string
 	Dirty  string
-	Tag    = "v0.8.0"
+	Tag    = "v0.9.0"
 )
 
 type Info struct {
--- scripts/install-Agent_b.ps1
+++ scripts/install-Agent_b.ps1
@@ -15,7 +15,7 @@ param(
 )
 
 $ErrorActionPreference = 'Stop'
-$displayVersion = '0.8.0'
+$displayVersion = '0.9.0'
 
 function Get-FullPath {
     param([string]$Path)
