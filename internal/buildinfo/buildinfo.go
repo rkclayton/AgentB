800bd97b4204e44ca3a25d9320be1439f1865182
rkclayton <56836047+rkclayton@users.noreply.github.com>
2026-09-09 17:16:24 -0500

    v0.20.0/W7: prepare release metadata

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -14,7 +14,7 @@ import (
 var (
 	Commit string
 	Dirty  string
-	Tag    = "v0.19.0"
+	Tag    = "v0.20.0"
 )
 
 type Info struct {
--- scripts/install-Agent_b.ps1
+++ scripts/install-Agent_b.ps1
@@ -16,7 +16,7 @@ param(
 )
 
 $ErrorActionPreference = 'Stop'
-$displayVersion = '0.19.0'
+$displayVersion = '0.20.0'
 
 function Get-FullPath {
     param([string]$Path)
