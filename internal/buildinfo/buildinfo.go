34f301f7b4c2d755cb3ee205bc7245b6f413421b
rkclayton <56836047+rkclayton@users.noreply.github.com>
2026-09-11 13:27:45 -0500

    v0.30.0/W9: stamp release version

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -14,7 +14,7 @@ import (
 var (
 	Commit string
 	Dirty  string
-	Tag    = "v0.29.0"
+	Tag    = "v0.30.0"
 )
 
 type Info struct {
--- scripts/install-Agent_b.ps1
+++ scripts/install-Agent_b.ps1
@@ -16,7 +16,7 @@ param(
 )
 
 $ErrorActionPreference = 'Stop'
-$displayVersion = '0.29.0'
+$displayVersion = '0.30.0'
 
 function Get-FullPath {
     param([string]$Path)
