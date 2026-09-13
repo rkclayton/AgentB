e453ef5fd7e79ad1b4834388236d67211918c772
rkclayton <56836047+rkclayton@users.noreply.github.com>
2026-09-12 20:21:51 -0500

    v0.45.0/W13: prepare release identity

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -14,7 +14,7 @@ import (
 var (
 	Commit string
 	Dirty  string
-	Tag    = "v0.44.0"
+	Tag    = "v0.45.0"
 )
 
 type Info struct {
--- scripts/install-Agent_b.ps1
+++ scripts/install-Agent_b.ps1
@@ -16,7 +16,7 @@ param(
 )
 
 $ErrorActionPreference = 'Stop'
-$displayVersion = '0.44.0'
+$displayVersion = '0.45.0'
 
 function Get-FullPath {
     param([string]$Path)
